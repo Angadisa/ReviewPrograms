ffd68c8b810ef85051bf5d24bd8c2471e2592933
Angadisa <sachinkumarangadi91@gmail.com>
2022-05-28 20:28:09 +0530

    update one file

--- pal.sh
+++ pal.sh
@@ -6,11 +6,6 @@ number=$n
 reverse=0
 while [ $n -gt 0 ]
 do
-a=`expr $n % 10 `
-n=`expr $n / 10 `
-reverse=`expr $reverse \* 10 + $a`
-done
-echo $reverse
 if [ $number -eq $reverse ]
 then
     echo "Number is palindrome"
@@ -18,5 +13,4 @@ else
     echo "Number is not palindrome"
 fi
 }
-r=`pal $n`
-echo "$r"
+
